fd89b16b6d723c4337b4d168f392ec90e0098004
kaysier23 <60122621+kaysier23@users.noreply.github.com>
2020-12-05 13:50:52 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,264 @@
+<html>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <body>
+  <h1>
+    <svg height="250" width="250">
+  <circle cx="100" cy="120" r="80" fill="purple" />
+  <line x1="70" x2="130" y1="60" y2="60" style="stroke:white; stroke-width:5" />
+  <line x1="60" x2="140" y1="180" y2="180" style="stroke:white; stroke-width:5" />
+
+  <line x1="71" x2="71" y1="59" y2="184" style="stroke:white;stroke-width:5" />
+  <line x1="128" x2="128" y1="59" y2="184" style="stroke:white;stroke-width:5" />
+
+  <!--windows 1 left -->
+
+  <line x1="83" x2="93" y1="76" y2="76" style="stroke:white; stroke-width:5" />
+  <line x1="83" x2="93" y1="96" y2="96" style="stroke:white; stroke-width:5" />
+  <line x1="81" x2="81" y1="74" y2="98" style="stroke:white;stroke-width:5" />
+  <line x1="95" x2="95" y1="74" y2="98" style="stroke:white;stroke-width:5" />
+
+  <!--windows 2 left -->
+
+  <line x1="83" x2="93" y1="106" y2="106" style="stroke:white; stroke-width:5" />
+  <line x1="83" x2="93" y1="126" y2="126" style="stroke:white; stroke-width:5" />
+  <line x1="81" x2="81" y1="104" y2="128" style="stroke:white;stroke-width:5" />
+  <line x1="95" x2="95" y1="104" y2="128" style="stroke:white;stroke-width:5" />
+
+  <!-- windows 3 left -->
+
+  <line x1="83" x2="93" y1="136" y2="136" style="stroke:white; stroke-width:5" />
+  <line x1="83" x2="93" y1="156" y2="156" style="stroke:white; stroke-width:5" />
+  <line x1="81" x2="81" y1="134" y2="158" style="stroke:white;stroke-width:5" />
+  <line x1="95" x2="95" y1="134" y2="158" style="stroke:white;stroke-width:5" />
+
+    <!-- windows 1 right -->  
+  <line x1="108" x2="118" y1="76" y2="76" style="stroke:white; stroke-width:5" />
+  <line x1="108" x2="118" y1="96" y2="96" style="stroke:white; stroke-width:5" />
+  <line x1="106" x2="106" y1="74" y2="98" style="stroke:white;stroke-width:5" />
+  <line x1="119" x2="119" y1="74" y2="98" style="stroke:white;stroke-width:5" />
+
+  <!-- windows 2 right -->
+  <line x1="108" x2="118" y1="106" y2="106" style="stroke:white; stroke-width:5" />
+  <line x1="108" x2="118" y1="126" y2="126" style="stroke:white; stroke-width:5" />
+  <line x1="106" x2="106" y1="104" y2="128" style="stroke:white;stroke-width:5" />
+  <line x1="119" x2="119" y1="104" y2="128" style="stroke:white;stroke-width:5" />
+
+  <!--windows 3 right -->
+
+  <line x1="108" x2="118" y1="136" y2="136" style="stroke:white; stroke-width:5" />
+  <line x1="108" x2="118" y1="156" y2="156" style="stroke:white; stroke-width:5" />
+  <line x1="106" x2="106" y1="134" y2="158" style="stroke:white;stroke-width:5" />
+  <line x1="119" x2="119" y1="134" y2="158" style="stroke:white;stroke-width:5" />
+</svg>
+  </h1>
+  <h2>
+    <!-- flag -->
+<svg>
+  <rect x="25" y="20" width="150" height="20" style="fill:white" />
+  <rect x="25" y="40" width="150" height="20" style="fill:white" />
+  <rect x="25" y="60" width="150" height="20" style="fill:yellow" />
+  <rect x="25" y="80" width="150" height="20" style="fill:green" />
+  <rect x="25" y="100" width="150" height="20" style="fill:white"/>
+  <rect x="25" y="120" width="150" height="20" style="fill:white" />
+   <circle cx="105" cy="80" r="20" fill="red" />
+</svg>
+  </h2>
+  <h3>
+    <svg height="500" width="500">
+  <polyline points="10,50 50,50 50,90 90,90 90,130 130,130 130,170 170, 170 170,210" 
+     style="fill:black;stroke:red;stroke-width:4;"/>
+      
+         <circle cx="37" cy="62" r="5" fill="white" />
+         <circle cx="65" cy="75" r="10" fill="white" />
+         <circle cx="77" cy="103" r="5" fill="white" />
+       <circle cx="105" cy="114" r="10" fill="white" />
+      <circle cx="117" cy="143" r="5" fill="white" />
+      <circle cx="145" cy="154" r="10" fill="white" />
+            <circle cx="157" cy="183" r="5" fill="white" />      
+</svg>
+  </h3>
+  <h4>
+    <svg height="700" width="500">
+  <polyline points="10,50 50,50 50,90 90,90 90,130 130,130 130,170" 
+     style="fill:blue;stroke:red;stroke-width:4;transform: translate(-25px, 200px) rotate(-45deg);"/>
+       <ellipse cx="74" cy="196" rx="10" ry="25" style="fill:green" />
+       <ellipse cx="131" cy="196" rx="10" ry="25" style="fill:green" />
+      <polyline points="10,50 50,50 50,90 90,90 90,130 130,130 130,170" 
+     style="fill:blue;stroke:red;stroke-width:4;transform: translate(-25px, 140px) rotate(-45deg);"/>
+      <ellipse cx="74" cy="137" rx="10" ry="25" style="fill:green" />
+       <ellipse cx="131" cy="137" rx="10" ry="25" style="fill:green" />
+      <polyline points="10,50 50,50 50,90 90,90 90,130 130,130 130,170" 
+     style="fill:blue;stroke:red;stroke-width:4;transform: translate(-25px, 80px) rotate(-45deg);"/>
+       <ellipse cx="74" cy="77" rx="10" ry="25" style="fill:green" />
+       <ellipse cx="131" cy="77" rx="10" ry="25" style="fill:green" />
+       <polyline points="10,50 50,50 50,90 90,90 90,130 130,130 130,170" 
+     style="fill:blue;stroke:red;stroke-width:4;transform: translate(-25px, 20px) rotate(-45deg);"/>
+        <!--right part small -->
+      <ellipse cx="159" cy="122" rx="10" ry="13" style="fill:yellow" />
+      <ellipse cx="159" cy="62" rx="10" ry="13" style="fill:yellow" />
+      <ellipse cx="159" cy="182" rx="10" ry="13" style="fill:yellow" />
+      <!--middle part small -->
+       <ellipse cx="102" cy="122" rx="10" ry="13" style="fill:yellow" />
+      <ellipse cx="102" cy="62" rx="10" ry="13" style="fill:yellow" />
+      <ellipse cx="102" cy="182" rx="10" ry="13" style="fill:yellow" />
+      
+      <!--left part small -->
+       <ellipse cx="46" cy="122" rx="10" ry="13" style="fill:yellow" />
+      <ellipse cx="46" cy="62" rx="10" ry="13" style="fill:yellow" />
+      <ellipse cx="46" cy="182" rx="10" ry="13" style="fill:yellow" />
+  </h4>
+    <h5>
+      <svg width="400" height="300">
+  <rect x="30" y="80" width="150" height="150" style="fill: red;stroke:brown;stroke-width:0;fill-opacity:0.1;stroke-opacity:0.9" />
+
+<line x1="30" x2="180" y1="60" y2="60"
+style="stroke:red;stroke-width:45" />
+        
+ <line x1="40" x2="40" y1="50" y2="150" style="stroke:red;stroke-width:20" />
+ <line x1="170" x2="170" y1="50" y2="150" style="stroke:red;stroke-width:20" />
+        
+ <circle cx="70" cy="110" r="10" fill="brown" />
+ <circle cx="140" cy="110" r="10" fill="brown" />
+
+ <line x1="70" x2="135" y1="145" y2="145" style="stroke:orange; stroke-width:25" />
+
+  <line x1="55" x2="155" y1="195" y2="195" style="stroke:brown; stroke-width:20" />
+</svg>
+    </h5>
+    <h6>
+      
+<!--bottom line-->
+      
+<svg height="700" width="500">
+  <polyline points="10,50 50,50 50,90 90,90 90,130 130,130 130,170" 
+     style="fill:red;stroke:green;stroke-width:4;transform: translate(-25px, 200px) rotate(-45deg);"/>
+  
+  <!--bottom 3stars-->
+  
+  <polygon class="star"
+        points="35,37.5  37.9,46.1 46.9,46.1 39.7,51.5 42.3,60.1 35,55  27.7,60.1 30.3,51.5 23.1,46.1 32.1,46.1"
+        style="fill: #ccffcc; stroke: green; transform: translate(11px, 169px);"/>
+   <polygon class="star"
+        points="35,37.5  37.9,46.1 46.9,46.1 39.7,51.5 42.3,60.1 35,55  27.7,60.1 30.3,51.5 23.1,46.1 32.1,46.1"
+        style="fill: #ccffcc; stroke: green; transform: translate(67px, 169px);"/>
+    <polygon class="star"
+        points="35,37.5  37.9,46.1 46.9,46.1 39.7,51.5 42.3,60.1 35,55  27.7,60.1 30.3,51.5 23.1,46.1 32.1,46.1"
+        style="fill: #ccffcc; stroke: green; transform: translate(123px, 169px);"/>
+  
+  <!-- bottom 2 lines -->
+  
+  <polyline points="10,50 50,50 50,90 90,90 90,130" 
+     style="fill:red;stroke:green;stroke-width:4;transform: translate(201.5px, 220px) rotate(135deg);"/>
+  
+    <!-- bottom 2 stars -->
+  
+   <polygon class="star"
+        points="35,37.5  37.9,46.1 46.9,46.1 39.7,51.5 42.3,60.1 35,55  27.7,60.1 30.3,51.5 23.1,46.1 32.1,46.1"
+        style="fill: #ccffcc; stroke: green; transform: translate(95px, 151px);"/>
+   <polygon class="star"
+        points="35,37.5  37.9,46.1 46.9,46.1 39.7,51.5 42.3,60.1 35,55  27.7,60.1 30.3,51.5 23.1,46.1 32.1,46.1"
+        style="fill: #ccffcc; stroke: green; transform: translate(40px, 151px);"/>
+
+<!-- 2nd to the last bottom 3 lines -->
+  
+  <polyline points="10,50 50,50 50,90 90,90 90,130 130,130 130,170" 
+     style="fill:red;stroke:green;stroke-width:4;transform: translate(-25px, 162px) rotate(-45deg);"/>
+  
+  <!-- 2nd to the last bottom 3 stars -->
+  
+   <polygon class="star"
+        points="35,37.5  37.9,46.1 46.9,46.1 39.7,51.5 42.3,60.1 35,55  27.7,60.1 30.3,51.5 23.1,46.1 32.1,46.1"
+        style="fill: #ccffcc; stroke: green; transform: translate(11px, 129px);"/>
+   <polygon class="star"
+        points="35,37.5  37.9,46.1 46.9,46.1 39.7,51.5 42.3,60.1 35,55  27.7,60.1 30.3,51.5 23.1,46.1 32.1,46.1"
+        style="fill: #ccffcc; stroke: green; transform: translate(67px, 129px);"/>
+    <polygon class="star"
+        points="35,37.5  37.9,46.1 46.9,46.1 39.7,51.5 42.3,60.1 35,55  27.7,60.1 30.3,51.5 23.1,46.1 32.1,46.1"
+        style="fill: #ccffcc; stroke: green; transform: translate(123px, 129px);"/>
+  
+  <!-- middle 2 lines -->
+  
+  <polyline points="10,50 50,50 50,90 90,90 90,130" 
+     style="fill:red;stroke:green;stroke-width:4;transform: translate(201.5px, 182px) rotate(135deg);"/>
+  
+    <!-- bottom 2 stars -->
+  
+   <polygon class="star"
+        points="35,37.5  37.9,46.1 46.9,46.1 39.7,51.5 42.3,60.1 35,55  27.7,60.1 30.3,51.5 23.1,46.1 32.1,46.1"
+        style="fill: #ccffcc; stroke: green; transform: translate(95px, 113px);"/>
+   <polygon class="star"
+        points="35,37.5  37.9,46.1 46.9,46.1 39.7,51.5 42.3,60.1 35,55  27.7,60.1 30.3,51.5 23.1,46.1 32.1,46.1"
+        style="fill: #ccffcc; stroke: green; transform: translate(40px, 113px);"/>
+  
+  <!-- middle 3 lines -->
+  
+  <polyline points="10,50 50,50 50,90 90,90 90,130 130,130 130,170" 
+     style="fill:red;stroke:green;stroke-width:4;transform: translate(-25px, 124px) rotate(-45deg);"/>
+  
+   <!-- upper middle 3 stars -->
+  
+   <polygon class="star"
+        points="35,37.5  37.9,46.1 46.9,46.1 39.7,51.5 42.3,60.1 35,55  27.7,60.1 30.3,51.5 23.1,46.1 32.1,46.1"
+        style="fill: #ccffcc; stroke: green; transform: translate(11px, 90px);"/>
+   <polygon class="star"
+        points="35,37.5  37.9,46.1 46.9,46.1 39.7,51.5 42.3,60.1 35,55  27.7,60.1 30.3,51.5 23.1,46.1 32.1,46.1"
+        style="fill: #ccffcc; stroke: green; transform: translate(67px, 90px);"/>
+    <polygon class="star"
+        points="35,37.5  37.9,46.1 46.9,46.1 39.7,51.5 42.3,60.1 35,55  27.7,60.1 30.3,51.5 23.1,46.1 32.1,46.1"
+        style="fill: #ccffcc; stroke: green; transform: translate(123px, 90px);"/>
+
+ <!-- super middle 2 lines -->
+  
+  <polyline points="10,50 50,50 50,90 90,90 90,130" 
+     style="fill:red;stroke:green;stroke-width:4;transform: translate(201.5px, 144px) rotate(135deg);"/>
+  
+   <!-- upper 2 stars -->
+  
+   <polygon class="star"
+        points="35,37.5  37.9,46.1 46.9,46.1 39.7,51.5 42.3,60.1 35,55  27.7,60.1 30.3,51.5 23.1,46.1 32.1,46.1"
+        style="fill: #ccffcc; stroke: green; transform: translate(95px, 76px);"/>
+   <polygon class="star"
+        points="35,37.5  37.9,46.1 46.9,46.1 39.7,51.5 42.3,60.1 35,55  27.7,60.1 30.3,51.5 23.1,46.1 32.1,46.1"
+        style="fill: #ccffcc; stroke: green; transform: translate(40px, 76px);"/>
+  
+   <!--  top 3 lines -->
+  
+  <polyline points="10,50 50,50 50,90 90,90 90,130 130,130 130,170" 
+     style="fill:red;stroke:green;stroke-width:4;transform: translate(-25px, 86px) rotate(-45deg);"/>
+  
+   <polygon class="star"
+        points="35,37.5  37.9,46.1 46.9,46.1 39.7,51.5 42.3,60.1 35,55  27.7,60.1 30.3,51.5 23.1,46.1 32.1,46.1"
+        style="fill: #ccffcc; stroke: green; transform: translate(11px, 55px);"/>
+   <polygon class="star"
+        points="35,37.5  37.9,46.1 46.9,46.1 39.7,51.5 42.3,60.1 35,55  27.7,60.1 30.3,51.5 23.1,46.1 32.1,46.1"
+        style="fill: #ccffcc; stroke: green; transform: translate(67px, 55px);"/>
+    <polygon class="star"
+        points="35,37.5  37.9,46.1 46.9,46.1 39.7,51.5 42.3,60.1 35,55  27.7,60.1 30.3,51.5 23.1,46.1 32.1,46.1"
+        style="fill: #ccffcc; stroke: green; transform: translate(123px, 55px);"/>
+  
+  <!-- super top 2 lines -->
+  
+  <polyline points="10,50 50,50 50,90 90,90 90,130" 
+     style="fill:red;stroke:green;stroke-width:4;transform: translate(201.5px, 106px) rotate(135deg);"/>
+  
+   <!-- upperupper 2 stars -->
+  
+   <polygon class="star"
+        points="35,37.5  37.9,46.1 46.9,46.1 39.7,51.5 42.3,60.1 35,55  27.7,60.1 30.3,51.5 23.1,46.1 32.1,46.1"
+        style="fill: #ccffcc; stroke: green; transform: translate(95px, 36px);"/>
+   <polygon class="star"
+        points="35,37.5  37.9,46.1 46.9,46.1 39.7,51.5 42.3,60.1 35,55  27.7,60.1 30.3,51.5 23.1,46.1 32.1,46.1"
+        style="fill: #ccffcc; stroke: green; transform: translate(40px, 36px);"/>
+
+ <!--  top line -->
+  
+  <polyline points="10,50 50,50 50,90" 
+     style="fill:red;stroke:green;stroke-width:4;transform: translate(31px, 48px) rotate(-45deg);"/>
+  
+  <polygon class="star"
+        points="35,37.5  37.9,46.1 46.9,46.1 39.7,51.5 42.3,60.1 35,55  27.7,60.1 30.3,51.5 23.1,46.1 32.1,46.1"
+        style="fill: #ccffcc; stroke: green; transform: translate(67px, 15px);"/>
+      </svg>
+    </h6>
+</body>
+    </html>
